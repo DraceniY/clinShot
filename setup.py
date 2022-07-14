0cb3c55ec8bad98f961857d2cd92f91b0f227c8b
yasmine <yasmine@mims.ai>
2022-07-14 14:58:26 -0400

    lint code

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from distutils.core import setup
+
 from Cython.Build import cythonize
 
-setup(ext_modules = cythonize("complementTools.pyx", nthreads=0, language_level = "3"))
+setup(ext_modules=cythonize("complementTools.pyx", nthreads=0, language_level="3"))
